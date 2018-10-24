a2aa47dacb69eb44246611bd81bc71e3d8e78be7
dacheaux <damjanspace@gmail.com>
2018-10-24 15:02:01 +0200

    refactor to OO code

--- src/index.js
+++ src/index.js
@@ -1,75 +1,94 @@
-const { shuffle, clone } = require('./helpers');
-
 const colors = [
 	['Red', '\x1b[31m'],
 	['Green', '\x1b[32m'],
 	['Yellow', '\x1b[33m'],
-	['Blue', '\x1b[1;34m'],
+	['Blue', '\x1b[34m'],
 	['Magenta', '\x1b[35m'],
 ];
-const reset = '\x1b[0m';
 
-const basicFighter = {
-	health: 100,
+class Unit {
+	constructor(name, color) {
+		this.health = 100;
+		this.attackDmg = this.damage;
+		this.name = name;
+		this.color = color;
+		this.message = '';
+	}
+
 	get rechargeTime() {
 		return (1000 * this.health) / 100;
-	},
+	}
+
 	get damage() {
 		return this.health / 100;
-	},
+	}
+
 	get criticalChance() {
 		return 10 - this.health / 10;
-	},
-};
-
-const fighters = colors.map(([name, color]) => (
-	Object.assign(clone(basicFighter), { name, color })
-));
-const shuffledFighters = shuffle(fighters);
-
-const attack = (unit) => {
-	const { name, color, health } = unit;
-	const unitIndex = fighters.indexOf(unit);
-
-	if (fighters.length < 2) {
-		clearInterval(unit.fight);
-		console.log(`${name} WON THE ROUND!`);
-		return;
 	}
-	const indexes = fighters.reduce(
-		(acc, v, i) => (i !== unitIndex ? [...acc, i] : acc),
-		[]
-	);
-	const enemyIndex = indexes[Math.floor(Math.random() * indexes.length)];
-	const enemy = fighters[enemyIndex];
 
-	const critical = unit.criticalChance >= Math.floor(Math.random() * 101)
-		? 'CRITICAL! '
-		: '';
-
-	const damage = critical ? unit.damage * 2 : unit.damage;
-	const unitString = `${critical}${color}${name} (${health.toFixed(2)} HP)`;
-
-	const enemyString = `${enemy.color}${enemy.name} (${enemy.health.toFixed(
-		2
-	)} HP)`;
-	console.log(
-		`${unitString} ${reset}hit ${enemyString} ${reset}for ${damage.toFixed(
+	logAttack(enemy, fighters) {
+		const reset = '\x1b[0m';
+		const {
+			name, color, message, health, attackDmg,
+		} = this;
+		const unitInfo = `${message}${color}${name} (${health.toFixed(
 			2
-		)} damage`
-	);
-	enemy.health -= damage;
-	if (enemy.health <= 0) {
-		clearInterval(enemy.fight);
-		fighters.splice(fighters.indexOf(enemy), 1);
-		console.log(`${reset}***Unit ${enemy.name} dieded***`);
-		console.log(`${name} destroyed ${enemy.name}`);
-		console.log(`There are ${fighters.length} fighters still left`);
+		)} HP)`;
+		const enemyInfo = `${enemy.color}${
+			enemy.name
+		} (${enemy.health.toFixed(2)} HP)`;
+		console.log(
+			`${unitInfo} ${reset}hit ${enemyInfo} ${reset}for ${attackDmg.toFixed(
+				2
+			)} damage`
+		);
+		if (enemy.health <= 0) {
+			const fightersLeft = fighters.filter(f => f.health > 0);
+			console.log(`${reset}***Unit ${enemy.name} dieded***`);
+			console.log(`${name} destroyed ${enemy.name}`);
+			console.log(`There are ${fightersLeft.length} fighters still left`);
+		}
 	}
-};
 
-const fight = () => shuffledFighters.forEach((unit) => {
-	unit.fight = setInterval(() => attack(unit), unit.rechargeTime);
-});
+	doDamage() {
+		const critical = this.criticalChance >= Math.floor(Math.random() * 101);
+		if (critical) {
+			this.attackDmg = this.damage * 2;
+			this.message = 'CRITICAL! ';
+		} else {
+			this.attackDmg = this.damage;
+			this.message = '';
+		}
+	}
 
-fight();
+	receiveDamage(dmg) {
+		this.health -= dmg;
+		if (this.health <= 0) {
+			clearInterval(this.inter);
+		}
+	}
+
+	attack(fighters) {
+		this.inter = setInterval(() => {
+			const enemies = fighters.filter(f => f !== this && f.health > 0);
+			if (!enemies.length) {
+				console.log(`${this.name} WON THE ROUND!`);
+				clearInterval(this.inter);
+			} else {
+				const enemy = enemies[Math.floor(Math.random() * enemies.length)];
+				this.doDamage();
+				enemy.receiveDamage(this.attackDmg);
+				this.logAttack(enemy, fighters);
+				clearInterval(this.inter);
+				if (this.health > 0 && fighters.length > 1) {
+					this.attack(fighters);
+				}
+			}
+		}, this.rechargeTime);
+	}
+}
+
+const fighters = colors.map(([name, color]) => new Unit(name, color));
+
+fighters.forEach(f => f.attack(fighters));
